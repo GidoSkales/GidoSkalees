7623db403f4d3d1c89fffd0bf4c4d0284be0658b
Gido Skales <66243914+GidoSkales@users.noreply.github.com>
2022-03-06 21:06:03 +0100

    Create app.js

--- app.js
+++ app.js
@@ -3,36 +3,28 @@ const container = document.querySelector("#cont");
 const button = document.querySelector(".btn");
 const display = document.querySelector(".show");
 
-const camelCase = (variable) => {
-    const splitted = variable.split('_');
-    const camelCase = splitted.map((word, index) => {
-        if (index === 0) {
-            return word;
-        }
-       
-       return !word[0] ? '' : word[0].toUpperCase() + word.slice(1);
-    }   ).join('');
 
-    return camelCase;
-}
 
 button.addEventListener("click", function () {
-	const text = container.value.toLowerCase().trim().split(" ");
-	// console.log(text);
-	
-	for (const [y, x] of text.entries()) {
-		if (x.includes("_")) {
-			const [c, v] = x.split("_");
-			const result = `${c}${v.replace(v[0], v[0].toUpperCase())}`;
-			// console.log(result);
-			display.textContent = result;
+	const camelCase = (variable) => {
+		const splitted = variable.split('_');
+		const camelCase = splitted.map((word, index) => {
+			if (index === 0) {
+				return word;
+			}
 			
-			document
-			.querySelector(".btn-clear")
-			.addEventListener("click", function () {
-				display.textContent = "";
-			});
-			return result;
-		}
+			return !word[0] ? '' : word[0].toUpperCase() + word.slice(1);
+		}   ).join('');
+		
+		return camelCase;
 	}
+	display.textContent = camelCase;
+	
+	document
+	.querySelector(".btn-clear")
+	.addEventListener("click", function () {
+		display.textContent = "";
+	});
+	
 });
+
